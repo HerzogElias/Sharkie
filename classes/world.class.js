89934250026922a3db2e7d99d15d54dc14ddd785
Elias Herzog <herzog@Eliass-Air.fritz.box>
2024-10-04 00:03:28 +0200

    eddit imige on canvas

--- classes/world.class.js
+++ classes/world.class.js
@@ -10,7 +10,12 @@ ctx;
         this.ctx=canvas.getContext('2d');   
         this.draw();
     }
+
     draw (){
+        let self = this;
         this.ctx.drawImage(this.charackter.img, this.charackter.x, this.charackter.y ,this.charackter.height, this.charackter.width)
+        requestAnimationFrame(function(){
+            self.draw();
+        });
     }
 }
