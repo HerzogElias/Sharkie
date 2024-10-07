e02bb4c4e88f04accf3130e4629bf4959eac9b20
Elias Herzog <herzog@Eliass-Air.fritz.box>
2024-10-07 21:09:32 +0200

    requestanimationframe()

--- classes/world.class.js
+++ classes/world.class.js
@@ -1,19 +1,24 @@
 class World {
-charackter =new Charackter();
-enemies = [
-    new Chicken(),
-    new Chicken(),
-    new Chicken (),
-];
-canvas;
-ctx;
-    constructor(canvas){
-        this.ctx=canvas.getContext('2d');
-        this.canvas=canvas;   
+    charackter = new Charackter();
+    enemies = [
+        new Chicken(),
+        new Chicken(),
+        new Chicken(),
+    ];
+    ctx;
+
+    constructor(canvas) {
+        this.ctx = canvas.getContext('2d');
+        this.draw();
 
     }
 
-    draw(){
-        this.ctx.drawImage(this.charackter.img, this.charackter.x, this.charackter.y,this.charackter.height) 
+    draw() {
+
+        this.ctx.drawImage(this.charackter.img, this.charackter.x, this.charackter.y, this.charackter.height, this.charackter.width);
+         let self = this;
+        requestAnimationFrame(function () {
+            self.draw();
+    });
     }
 }
