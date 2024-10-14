918b30c51da8c5cf4ae63dca630b44441c5397ec
Elias Herzog <herzog@Eliass-MacBook-Air.fritz.box>
2024-10-14 16:51:07 +0200

    cloud animation

--- classes/cloud.class.js
+++ classes/cloud.class.js
@@ -1,7 +1,15 @@
-class Cloud  extends MovableObject{
-    x=150+Math.random()*500;
-    y=25;
-    constructor(){
+class Cloud extends MovableObject {
+    x = 150 + Math.random() * 500;
+    y = 25;
+
+    constructor() {
         super().loadImage('./img/3. Background/Legacy/Layers/1. Light/2.png');
-    }     
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => { // Korrektur hier: das "=>" sollte nicht in Klammern stehen
+            this.x -= 0.15;
+        }, 1000 / 60);
+    }
 }
