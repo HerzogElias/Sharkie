27a92b66e49cac600e7aa338f8072a673f64e2f7
Elias Herzog <herzog@Eliass-Air.fritz.box>
2024-10-15 18:54:11 +0200

    added keyboard on world

--- js/game.js
+++ js/game.js
@@ -1,10 +1,58 @@
 let canves;
 let world;
- 
+let keyboard = new Keyboard();
+
 function init() {
     let canvas = document.getElementById('canvas');
-    world= new World(canvas);   
+    world= new World(canvas, keyboard);   
     let ctx = canvas.getContext('2d'); // Korrigierte Anführungszeichen und "canvas"
     
     console.log('My character is', world.charackter);
 }
+window.addEventListener("keydown",(e) => {
+    console.log(e)
+
+    if (e.keyCode == 32 ) {
+        keyboard.SPACE=true;
+    }
+
+    if (e.keyCode == 37 ) {
+        keyboard.LEFT=true;
+    }
+
+    if (e.keyCode == 38 ) {
+        keyboard.UP=true;
+    }
+
+    if (e.keyCode == 39 ) {
+        keyboard.RIGHT=true;
+    }
+    if (e.keyCode == 40 ) {
+        keyboard.DOWN=true;
+    }
+});
+
+
+window.addEventListener("keyup",(e) => {
+    console.log(e)
+
+    if (e.keyCode == 32 ) {
+        keyboard.SPACE=false;
+    }
+
+    if (e.keyCode == 37 ) {
+        keyboard.LEFT=false;
+    }
+
+    if (e.keyCode == 38 ) {
+        keyboard.UP=false;
+    }
+
+    if (e.keyCode == 39 ) {
+        keyboard.RIGHT=false;
+    }
+    if (e.keyCode == 40 ) {
+        keyboard.DOWN=false;
+    }
+});
+
