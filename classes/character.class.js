7806adcbff150d9143bede79ab32a73ff28ef754
Elias Herzog <herzog@Eliass-Air.fritz.box>
2024-10-16 11:52:24 +0200

    charcakter can walk in all directions

--- classes/character.class.js
+++ classes/character.class.js
@@ -1,6 +1,7 @@
 class Charackter extends MovableObject{
     height=100;
     width=200;
+    speed=10;
 
     IMAGES_WALKING=
         [
@@ -12,7 +13,7 @@ class Charackter extends MovableObject{
             './img/1.Sharkie/3.Swim/6.png'
         ]
   
-
+    world;
     constructor(){
         super().loadImage('./img/1.Sharkie/2.Long_IDLE/i1.png')
         this.loadImages(this.IMAGES_WALKING)
@@ -21,11 +22,30 @@ class Charackter extends MovableObject{
 
     animate(){
         setInterval(() => {
+            if(this.world.keyboard.RIGHT) {
+                this.x +=this.speed;
+            }
+
+            if(this.world.keyboard.LEFT) {
+                this.x -=this.speed;
+            }
+
+            if(this.world.keyboard.DOWN) {
+                this.y +=this.speed;
+            }
+
+            if(this.world.keyboard.UP) {
+                this.y -=this.speed;
+            }
+        }, 1000/60);
+
+        
+        setInterval(() => {
         let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = this.IMAGES_WALKING[i];
         this.img=this.imageCache[path];
         this.currentImage++;
-        },1000)
+        },200)
     }
 
     jump(){}
