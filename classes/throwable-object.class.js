96786d3f46edf7bf5a252ee2969743cd1ce46aac
Elias Herzog <elias.herzog.02@gmail.com>
2024-11-06 22:59:12 +0100

    deleate comments on throwable object

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -41,28 +41,3 @@ class ThrowableObject extends MovableObject {
         }, 1000 / 25);
     }
 }
-
-    /*
-    constructor(){
-        super();
-        this.world=world;
-        this.width = 200;
-        this.height = 60;
-        this.loadImage('img/1.Sharkie/4.Attack/Bubble trap/Bubble.png')
-        this.throwBubble(x,y);
-       
-    }
-    
-    throwBubble(x,y) {
-        console.log('Tetschen');
-        let bubbleX = x.x + x.width;
-        this.bubbleY = y.y + y.height * 0.5;
-        
-        setInterval(() => {
-            this.bubbleY -= this.bubbleSpeedY;
-            this.bubbleSpeedY -= this.gravity;
-            if (this.bubbleSpeedY < -5) {
-                this.bubbleSpeedY = -5;
-            }
-        }, 1000 / 60);
-}*/
