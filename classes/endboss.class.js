ce73524dc78a17833c25a2cbdcaa8dbc548badee
Elias Herzog <elias.herzog.02@gmail.com>
2024-11-11 22:35:45 +0100

    show startscreen and create new game on canvas

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -1,33 +1,55 @@
-class Endboss extends MovableObject{
-    width= 350;
-    height=350;
-    IMAGES_WALKING=[
-        'img/2.Enemy/3 Final Enemy/2.floating/1.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/2.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/3.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/4.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/5.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/6.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/7.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/8.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/9.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/10.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/11.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/12.png',
-        'img/2.Enemy/3 Final Enemy/2.floating/13.png'
-    ]
+class Endboss extends MovableObject {
+    width = 350;
+    height = 350;
+    IMAGES_WALKING =
+        [
+            'img/2.Enemy/3 Final Enemy/2.floating/1.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/2.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/3.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/4.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/5.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/6.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/7.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/8.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/9.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/10.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/11.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/12.png',
+            'img/2.Enemy/3 Final Enemy/2.floating/13.png'
+        ]
 
-    constructor(){
+    IMAGES_FIRST_ANIMATEON =
+        [
+            'img/2.Enemy/3 Final Enemy/1.Introduce/1.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/2.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/3.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/4.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/5.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/6.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/7.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/8.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/9.png',
+            'img/2.Enemy/3 Final Enemy/1.Introduce/10.png'
+        ]
+    constructor() {
         super().loadImage(this.IMAGES_WALKING[0]);
         this.loadImages(this.IMAGES_WALKING);
-        this.x=2000;
-        this.y=50;
+        this.loadImages(this.IMAGES_FIRST_ANIMATEON);
+        this.x = 2000;
+        this.y = 50;
         this.animate();
     }
 
-    animate(){
+    animate() {
         setInterval(() => {
-           this.playAnimation(this.IMAGES_WALKING)
-        },200)
+            this.playAnimation(this.IMAGES_WALKING)
+        }, 200)
+    }
+
+    endbossFirstAnimation() {
+        if (this.world.charackter.x < -1440) {
+            console.log(this.x);
+            this.playAnimation(this.IMAGES_FIRST_ANIMATEON);
+        }
     }
 }
--- js/game.js
+++ js/game.js
@@ -4,63 +4,95 @@ let keyboard = new Keyboard();
 
 function init() {
     let canvas = document.getElementById('canvas');
-    world= new World(canvas, keyboard);   
-    let ctx = canvas.getContext('2d'); // Korrigierte Anführungszeichen und "canvas"
-    
+    world = new World(canvas, keyboard);
+    let ctx = canvas.getContext('2d');
     console.log('My character is', world.charackter);
+    showGame();
 }
-window.addEventListener("keydown",(e) => {
-   /* console.log(e)*/
 
-    if (e.keyCode == 32 ) {
-        keyboard.SPACE=true;
+window.addEventListener("keydown", (e) => {
+    /* console.log(e)*/
+
+    if (e.keyCode == 32) {
+        keyboard.SPACE = true;
     }
 
-    if (e.keyCode == 37 ) {
-        keyboard.LEFT=true;
+    if (e.keyCode == 37) {
+        keyboard.LEFT = true;
     }
 
-    if (e.keyCode == 38 ) {
-        keyboard.UP=true;
+    if (e.keyCode == 38) {
+        keyboard.UP = true;
     }
 
-    if (e.keyCode == 39 ) {
-        keyboard.RIGHT=true;
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = true;
     }
-    if (e.keyCode == 40 ) {
-        keyboard.DOWN=true;
+    if (e.keyCode == 40) {
+        keyboard.DOWN = true;
     }
 
-    if (e.keyCode ==68) {
-        keyboard.D=true;
+    if (e.keyCode == 68) {
+        keyboard.D = true;
+    }
+});
+
+
+window.addEventListener("keyup", (e) => {
+    /*    console.log(e)*/
+
+    if (e.keyCode == 32) {
+        keyboard.SPACE = false;
+    }
+
+    if (e.keyCode == 37) {
+        keyboard.LEFT = false;
+    }
+
+    if (e.keyCode == 38) {
+        keyboard.UP = false;
+    }
+
+    if (e.keyCode == 39) {
+        keyboard.RIGHT = false;
+    }
+    if (e.keyCode == 40) {
+        keyboard.DOWN = false;
+    }
+
+    if (e.keyCode == 68) {
+        keyboard.D = false;
     }
 });
 
+function showStartscreen() {
+    document.getElementById('canvas').classList.add('dnone');
+    document.getElementById('startscreen').classList.remove('dnone');
+    document.getElementById('youwon').classList.add('dnone');
+    document.getElementById('youlost').classList.add('dnone');
+}
 
-window.addEventListener("keyup",(e) => {
-/*    console.log(e)*/
+function showGame() {
+    document.getElementById('canvas').classList.remove('dnone');
+    document.getElementById('startscreen').classList.add('dnone');
+    document.getElementById('youwon').classList.add('dnone');
+    document.getElementById('youlost').classList.add('dnone');
+}
 
-    if (e.keyCode == 32 ) {
-        keyboard.SPACE=false;
-    }
+function showWinnerScreen() {
+    document.getElementById('canvas').classList.add('dnone');
+    document.getElementById('startscreen').classList.add('dnone');
+    document.getElementById('youwon').classList.remove('dnone');
+    document.getElementById('youlost').classList.add('dnone');
+}
 
-    if (e.keyCode == 37 ) {
-        keyboard.LEFT=false;
-    }
+function schowLostscreen() {
+    document.getElementById('canvas').classList.add('dnone');
+    document.getElementById('startscreen').classList.add('dnone');
+    document.getElementById('youwon').classList.add('dnone');
+    document.getElementById('youlost').classList.remove('dnone');
+}
 
-    if (e.keyCode == 38 ) {
-        keyboard.UP=false;
-    }
 
-    if (e.keyCode == 39 ) {
-        keyboard.RIGHT=false;
-    }
-    if (e.keyCode == 40 ) {
-        keyboard.DOWN=false;
-    }
 
-    if (e.keyCode ==68) {
-        keyboard.D=false;
-    }
-});
 
